ed5d20bc8777e9c5b1fb1196a4c052b04bea8eab
smartflomeet <atul.titare@tatatel.co.in>
2025-04-03 17:18:17 +0530

    Initial Commit (Internal files)

---
+++ sample-csharp/Models/EnablexConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace EnableX.Models
+{
+    /// <summary>
+    /// config object model
+    /// </summary>
+    public class EnablexConfig
+    {
+        // Server Video API URL 
+        public string API_URL { get; set; }
+        // APP ID to use service
+        public string APP_ID { get; set; }
+        // APP KEY to use service
+        public string APP_KEY { get; set; }
+    }
+}
---
+++ sample-csharp/Models/RoomBodySettings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace EnableX.Models
+{
+    /// <summary>
+    /// create room settings object model
+    /// </summary>
+    class RoomBodySettings
+    {
+        // room is a permanent room or scheduled for a specific duration
+        public bool scheduled { get; set; }
+        // whether room is adhoc or scheduled
+        public bool adhoc { get; set; }
+        // Total Number of moderators allowed in the Room
+        public string moderators { get; set; }
+        // Total Number of Participants allowed in the Room
+        public string participants { get; set; }
+        // Total Duration of Room
+        public string duration { get; set; }
+        // Quality of the video in session.
+        public string quality { get; set; }
+        // If auto-recording is enabled or disabled
+        public bool auto_recording { get; set; }
+
+    }
+}
